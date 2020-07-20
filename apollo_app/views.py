fb30e7cb2e26317108797f63b661d7d0810d629c
2670211238@qq.com <2670211238@qq.com>
2020-07-20 15:09:54 +0800

    增加了登录功能

--- apollo_app/views.py
+++ apollo_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('index首页')
+
+def login(request):
+    return HttpResponse('这是登录界面')
